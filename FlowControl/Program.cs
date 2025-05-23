3b93a9db9464b6162c61a8c84aaf6360cf616657
Felix Cederblad <felix_cederblad@hotmail.com>
2025-05-23 14:06:31 +0200

    Add project files.

---
+++ FlowControl/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace FlowControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Välkommen till Huvudmenyn. Navigera denna genom att skriva in" +
+                "rätt siffra till rätt funktion");
+
+            bool running = true;
+
+            while (running)
+            {
+                int userInput = 0;
+                switch (userInput)
+                {
+                    case 0:
+                        Console.WriteLine("Nu stängs programmet");
+                        break;
+                    default:
+                        Console.WriteLine("Felaktig input");
+                        break;
+                }
+
+            }
+        }
+    }
+}
